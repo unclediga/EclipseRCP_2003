37b3738ba80005dfade89043ddeed42740cb3fa7
BorodinovI <igordin@yandex.ru>
2014-04-17 18:00:34 +0400

    Новая схема активатора. Спёр из примеров 2014 года.

--- src/org/eclipse/contribution/junit/JUnitPlugin.java
+++ src/org/eclipse/contribution/junit/JUnitPlugin.java
@@ -10,20 +10,32 @@ import org.eclipse.jdt.core.IType;
 import org.osgi.framework.BundleContext;
 
 public class JUnitPlugin extends Plugin {
+	
 	private static JUnitPlugin instance;
+	
 	private ArrayList listeners = new ArrayList();
 
-	public JUnitPlugin(IPluginDescriptor descriptor) {
-		super(descriptor);
-		instance = this;
-		System.out
-				.println("MY:" + this.getClass().getName() + ".Constructor()");
+	public JUnitPlugin(){
+		
 	}
 
 	public static JUnitPlugin getPlugin() {
 		return instance;
 	}
+	
+	@Override
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		instance = this;
+	}
 
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		instance = null;
+		super.stop(context);
+	}
+
+	////////////////////////////////////////////////////////////
 	public void run(IType type) {
 		new TestRunner().run(type);
 	}
